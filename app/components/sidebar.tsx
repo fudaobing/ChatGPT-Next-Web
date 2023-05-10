bc4b3d08cad529e69a2eefc49f4dcb6c5df80dd5
fudaobing <31214245+fudaobing@users.noreply.github.com>
2023-05-10 18:27:15 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT AI 助手</div>
+        <div className={styles["sidebar-title"]}>ChatGPT IMiao</div>
         <div className={styles["sidebar-sub-title"]}>
           QQ：3511181996
         </div>
